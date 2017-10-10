8b1c2850b6b770192377a3ab5c3ad638722de1a9
mridulp <mridulkumar.it@gmail.com>
2017-10-10 15:32:03 +0530

    Project setup done

---
+++ optybackend/src/main/java/com/spm/tool/optybackend/App.java
@@ -0,0 +1,13 @@
+package com.spm.tool.optybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
